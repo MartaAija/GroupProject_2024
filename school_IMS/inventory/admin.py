a8392145222b6b591b7333942abc1071cd74287a
Rmina <eminaashe@gmail.com>
2024-04-23 12:02:20 +0100

    [commit message]

--- school_IMS/inventory/admin.py
+++ school_IMS/inventory/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Equipment
+from .models import Equip_Item
 
 # Register your models here.
 admin.site.register(Equipment)
+admin.site.register(Equip_Item)
--- school_IMS/inventory/views.py
+++ school_IMS/inventory/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from .models import Equipment
+from django.core.paginator import Paginator
 
 # Create your views here.
 def all_equipment(request):
@@ -9,11 +10,17 @@ def all_equipment(request):
         # Retrieve the search query entered by the user
         search_query = request.POST['search_query']
         # Filter your model by the search query
-        equipments = Equipment.objects.filter(display_name__contains=search_query)
+        p = Paginator(Equipment.objects.filter(display_name__contains=search_query), 7)
+        page = request.GET.get('page')
+        equipments = p.get_page(page)
+
         return render(request, 'inventory/inventory.html', {'query':search_query, 'equipments': equipments})
     else:
-        equipments = Equipment.objects.all()
-        return render(request, 'inventory/inventory.html',{ 'equipments': equipments})
+        p = Paginator(Equipment.objects.all(), 7)
+        page = request.GET.get('page')
+        equipments = p.get_page(page)
+
+        return render(request, 'inventory/inventory.html',{ 'equipments': equipments, })
 
 def detail(request, equipment_id):
     equipment = get_object_or_404(Equipment,pk=equipment_id)
